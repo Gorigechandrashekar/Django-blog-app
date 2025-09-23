ad9332dcc1bb025e1529e126a4bf5a8b21933fbd
chandrashekar gorige <gchandrashekar57@gmail.com>
2025-09-23 11:04:41 +0530

    static added

--- members/urls.py
+++ members/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include,path
 from . import views
 urlpatterns=[
-    path("admin/",admin.site.urls),
+    path("",include('users.urls')),
     path("",views.main,name="main"),
     path("members/",views.members,name='members'),
     path("members/details/<int:id>",views.details,name="details"),
